aaec81d2c6f338c0cff5094ae692fb1823f6c892
Edoardo <pieterevd@gmail.com>
2025-11-14 12:26:22 +0100

    Add main.c

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+/*
+ * Allocator code
+ */
+
+// pool and block size
+#define POOL_SIZE 4096 // 4kb memory pool
+#define MIN_BLOCK_size // Minimum seze for splitting
+
+// Block header structure
+// Metadata for each memory block
+static char memory_pool[100];
+
+typedef struct block_header {
+    size_t size;                // Size without header
+    int is_free;                // Boolean
+    struct block_header* next;  // Pointer to next block in the list
+} block_header_t
+
+
+static char memory_pool[POOL_SIZE];
+static block_header_t* free_list_head = NULL;
+static int initialized = 0; // Boolean
+
+// Allocator initiation
+void init_allocator() {
+    if (initialized) return; // skip if true
+
+    // Treat start of pool as the first header
+    free_list_head = (block_header_t*)memory_pool;
+    free_list_head->size = POOL_SIZE - sizeof(block_header_t);
+    free_list_head->is_free = 1; // true
+    free_list_head->next = NULL;
+
+    initialized = 1; // set to true
+    printf("[INIT] Allocator initialized with %zu bytes\n", free_list_head->H)
+}
+
+// Custom malloc implementation
+
+// Custom free implementation
+
+// Debug function to print memory state
+
+/*
+ * Test program code
+ */
+
